8626550ae16b7a5b0c5781893f341e373d16d253
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-11 21:08:42 +0500

    Enter array

---
+++ Example011_EnterArray/Program.cs
@@ -0,0 +1,32 @@
+﻿// Создание функции/метода заполнения массива
+
+// функция заполнения массива
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 30);
+        index += 1;
+    }
+}
+
+// функция вывод массива на экран
+void ShowArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < coll.Length)
+    {
+        Console.WriteLine(coll[position]);
+        position += 1;
+    }
+}
+
+//создаем массив под названием array из 10 элементов
+int[] array = new int[10];
+
+//Применяем к нему заполнению и выводу на экран указанные функции
+FillArray(array);
+ShowArray(array);
