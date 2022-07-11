a1e142e8e617663c47aa757054cdc2782caa76ab
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-11 21:40:01 +0500

    question

---
+++ Example011_2_FindNumberInArray/Program.cs
@@ -0,0 +1,56 @@
+﻿// Создание функции/метода заполнения массива
+
+// функция заполнения массива
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 30);
+        index += 1;
+    }
+}
+
+// функция вывод массива на экран
+void ShowArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < coll.Length)
+    {
+        Console.WriteLine(coll[position]);
+        position += 1;
+    }
+}
+
+// функция по по поиску и выводу индекса искомового элемента массива
+int IndexOf(int[] collection, int found)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index]) == found;
+        {
+            position = index;
+        }
+        index += 1;
+    }
+    return position;
+}
+
+
+//создаем массив под названием array из 10 элементов
+int[] array = new int[10];
+
+//Применяем к нему заполнению и выводу на экран указанные функции
+FillArray(array);
+ShowArray(array);
+// пропускаем строку
+Console.WriteLine();
+
+//находим искомый элемент с помощью функции
+int pos = IndexOf(array, 5);
+Console.WriteLine(pos);
