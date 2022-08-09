abd0ea6e186aa64bd02b0ba7706f9248278dc385
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-09 10:49:54 +0500

    015

---
+++ Example015_Rekurs/Program.cs
@@ -0,0 +1,28 @@
+﻿// Рекурсия для факториала
+int Factorial (int n)
+{
+    if (n == 1) 
+        return 1;
+    else 
+        return n * Factorial(n-1);
+}
+
+Console.WriteLine(Factorial(5));
+
+
+// Последовательность Фибоначчи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci (int num)
+{
+    if (num == 1 || num == 2 ) return 1;
+    else 
+    return Fibonacci (num-1) + Fibonacci (num - 2);
+}
+
+for (int i = 1; i<10; i++)
+{
+    Console.WriteLine (Fibonacci (i));
+}
