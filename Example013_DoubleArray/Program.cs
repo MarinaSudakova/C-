024e839aef3d610e94120fd23d946cb7799275d6
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-09 10:12:13 +0500

    013

---
+++ Example013_DoubleArray/Program.cs
@@ -0,0 +1,43 @@
+﻿// Создаем таблицу, 2 строчки 5 столбцов
+
+string[,] table = new string[2, 5];
+table [1, 2] = "слово";
+
+//for (int rows=0; rows<2; rows++)
+//{
+//    for (int columns=0; columns<5; columns++)
+//    {
+//        Console.WriteLine($"-{table[rows, columns]}-");
+//    }
+//}
+
+int[,] matrix = new int[3, 4];
+
+void PrintArray (int[,] matr)
+{
+    for (int rows=0; rows<matr.GetLength(0); rows++)
+    {
+        for (int columns=0; columns<matr.GetLength(1); columns++)
+        {
+            Console.Write ($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int[,] matr)
+{
+    for (int rows=0; rows<matr.GetLength(0); rows++)
+    {
+        for (int columns=0; columns<matr.GetLength(1); columns++)
+        {
+            matr[rows, columns] = new Random().Next (1,10);
+        }
+    }
+}
+
+PrintArray(matrix);
+FillArray (matrix);
+Console.WriteLine();
+PrintArray (matrix);
+
