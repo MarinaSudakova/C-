a1c063af725ec6ca99753b153e85dd4c8d77775b
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-28 15:27:53 +0500

    016 Rekurs

--- Example016_Rekurs2/Program.cs
+++ Example016_Rekurs2/Program.cs
@@ -16,23 +16,23 @@ int PowerRec (int a, int n)
 }
 
 // Все возможные слова из символов
-// int n=1;
-// void FindWords (string alphabet, char[] word, int length = 0);
-// {
-//     if (length == word.length)
-//     {
-//         Console.WriteLine ($" {n++} {new String(word)} "); 
-//         return;
-//     }
 
-//     for (int i = 0; i < alphabet.Length; i++)
-//     {
-//         word[length] = alphabet[i];
-//         FindWords(alphabet, word, length + 1);
-//     }
-// }
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+ {
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}");  return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+ 
 
-//FindWords ("аисв", new char[3]);
+FindWords("аисв", new char[5]);
 
 //Получаем доступ к директории и информацию о ней
 
